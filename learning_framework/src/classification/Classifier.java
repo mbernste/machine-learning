7a56e124e12745962ffc437687501910c673314f
Matthew Bernstein <berniematt18@gmail.com>
2013-12-13 11:34:45 -0600

    Improved javadocs

--- learning_framework/src/classification/Classifier.java
+++ learning_framework/src/classification/Classifier.java
@@ -2,8 +2,15 @@ package classification;
 
 import data.DataSet;
 
+/**
+ * Any model learned to perform a supervised classificiation task should 
+ * implement this interface so that it can be integrated into an experimental/
+ * learning framework.
+ * 
+ * @author Matthew Bernstein - matthewb@cs.wisc.edu
+ *
+ */
 public interface Classifier 
 {
     public ClassificationResult classifyData(DataSet testData); 
-
 }
