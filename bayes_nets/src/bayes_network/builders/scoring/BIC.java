551034bf4962c246bc55b4e31a1b018f5a7c3304
Matthew Bernstein <berniematt18@gmail.com>
2013-12-14 14:09:42 -0600

    Implemented Topological Sort on DAG

--- bayes_nets/src/bayes_network/builders/scoring/BIC.java
+++ bayes_nets/src/bayes_network/builders/scoring/BIC.java
@@ -3,6 +3,14 @@ package bayes_network.builders.scoring;
 import data.DataSet;
 import bayes_network.BayesianNetwork;
 
+/**
+ * Scores a Bayesian network against a dataset using Bayesian Information 
+ * Criterion (BIC).
+ * 
+ * 
+ * @author Matthew Bernstein - matthewb@cs.wisc.edu
+ *
+ */
 public class BIC implements ScoringFunction
 {
     public Double scoreNet(BayesianNetwork net, DataSet data)
