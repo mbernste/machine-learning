c80a14f44fe29bd6eb71415ed9ca7259f7cd9946
Matthew Bernstein <berniematt18@gmail.com>
2013-12-22 21:22:37 -0600

    Deleted reduntant project

--- bayes_nets/src/bayes_network/builders/scoring/ScoringFunction.java
+++
@@ -1,9 +0,0 @@
-package bayes_network.builders.scoring;
-
-import bayes_network.BayesianNetwork;
-import data.DataSet;
-
-public interface ScoringFunction 
-{
-    public Double scoreNet(BayesianNetwork net, DataSet data);
-}
